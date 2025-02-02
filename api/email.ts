7528ee53420bd0ba169c78b2f82d3e94b5216e30
Atanas Atanasov <scriptex.bg@gmail.com>
2025-02-02 10:43:42 +0200

    Debug

--- api/email.ts
+++ api/email.ts
@@ -11,6 +11,7 @@ export default async function handler(_: VercelRequest, res: VercelResponse) {
 
 	try {
 		const { ALERTS_PAGE, DATE_SELECTOR, CHILD_CLASSNAME, ALERTS_SELECTOR } = process.env;
+		console.log(ALERTS_PAGE);
 		const pages = JSON.parse(ALERTS_PAGE!);
 		const alerts = [];
 
@@ -31,6 +32,7 @@ export default async function handler(_: VercelRequest, res: VercelResponse) {
 
 		return res.status(200).send(!content ? 'No updates found!' : 'Email sent!');
 	} catch (error) {
+		console.log(error);
 		return res.status(500).send(error);
 	}
 }
